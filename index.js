7e33217f8845d5f3fd01b3d5074f54cda91cfbbb
Jganyo-dotcom <elikemejay@gmail.com>
2026-04-19 17:56:30 +0000

    feat: pages rendering

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const BaseApi = "http://127.0.0.1:4444";
-  //const BaseApi = "https://medsec.onrender.com";
+  //const BaseApi = "http://127.0.0.1:4444";
+  const BaseApi = "https://medsec.onrender.com";
   const token = localStorage.getItem("authToken"); // saved at login
 
   // DOM references
